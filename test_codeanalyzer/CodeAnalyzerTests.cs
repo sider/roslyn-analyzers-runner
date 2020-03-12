2e95376bf71542f34662cd2ce2d760e1cdc35412
suetsuguk <suetsuguk@toyo.co.jp>
2020-03-12 17:13:34 +0900

    空の単体テストプロジェクトを追加

---
+++ test_codeanalyzer/CodeAnalyzerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test_codeanalyzer
+{
+	[TestClass]
+	public class CodeAnalyzerTests
+	{
+		[TestMethod]
+		public void Test_Diagnose()
+		{
+			Assert.IsTrue(false);
+		}
+	}
+}
